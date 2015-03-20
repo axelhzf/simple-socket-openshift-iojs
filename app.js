fa496c00381b38d621fb9f1260e2a8af556cd6d7
Axel Hernández Ferrera <axelhzf@gmail.com>
2015-03-20 19:33:19 +0000

    remove intervals on disconnect

--- app.js
+++ app.js
@@ -1,37 +1,28 @@
-//var app = require('http').createServer(handler);
 var koa = require("koa");
-
 var fs = require('fs');
 
 var ip = process.env.OPENSHIFT_IOJS_IP || "127.0.0.1";
-var port = process.env.OPENSHIFT_IOJS_PORT || 3000;
+var port = process.env.OPENSHIFT_IOJS_PORT || 8000;
 
 var app = koa();
-
 
 app.use(function* () {
   var file = fs.readFileSync(__dirname + '/index.html');
   this.set("content-type", "text/html");
-  this.body =  file;
+  this.body = file;
 });
-
 
 var server = app.listen(port, ip);
 
 var io = require('socket.io')(server);
-io.set('transports', ['websocket',
-  'flashsocket',
-  'htmlfile',
-  'xhr-polling',
-  'jsonp-polling',
-  'polling']);
 
 io.on('connection', function (socket) {
-  setInterval(function () {
-    socket.emit('news', {hello: 'world'});
+  var interval = setInterval(function () {
+    socket.emit('news', {hello: 'world', at: new Date()});
   }, 100);
 
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+  socket.on("disconnect", function () {
+    clearInterval(interval);
+  })
+
 });
